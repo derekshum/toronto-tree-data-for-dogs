68ad0d05187c7543da3211c8da97af4093f31381
Derek Shum <derekqlshum@gmail.com>
2026-04-26 13:15:28 -0400

    Improve performance by analysing territory trees in pandas

--- generate_analysis.py
+++ generate_analysis.py
@@ -1,44 +1,34 @@
 from sqlalchemy import create_engine
 from sqlalchemy.engine import Engine
 import pandas as pd
+from datetime import datetime
 
-def get_tree_count_in_territory(engine: Engine, ids: list[int]) -> int:
-    """Get the total number of trees within the territory defined by tree ids provided"""
+def get_trees_in_territory(engine: Engine, ids: list[int]) -> pd.DataFrame:
+    """Get the dataframe for trees within the territory defined by tree ids provided"""
     query = """
         WITH territory AS (
             SELECT ST_ConvexHull(ST_Collect(geometry)) AS polygon
             FROM trees
             WHERE id = ANY(%(ids)s)
         )
-        SELECT COUNT(*) AS count
+        SELECT *
         FROM trees
         JOIN territory
         ON ST_Intersects(trees.geometry, territory.polygon)
     """
-    df = pd.read_sql(query, engine, params={"ids": ids})
-    return df.iloc[0]["count"]
+    return pd.read_sql(query, engine, params={"ids": ids})
 
-def get_unique_list(engine: Engine, ids: list[int], property: str) -> list[str]:
+def get_unique_list(df: pd.DataFrame, property: str) -> list[str]:
     """Get the alphabetized list of all the unique values in column of the 
     property provided"""
-    query = f"""
-        WITH territory AS (
-            SELECT ST_ConvexHull(ST_Collect(geometry)) AS polygon
-            FROM trees
-            WHERE id = ANY(%(ids)s)
-        )
-        SELECT DISTINCT {property}
-        FROM trees
-        JOIN territory
-        ON ST_Intersects(trees.geometry, territory.polygon)
-        GROUP BY {property}
-        ORDER BY {property}
-    """
-    df = pd.read_sql(query, engine, params={"ids": ids, "property": property})
-    return df[property].tolist()
+    return sorted(df[property].unique())
 
+startTime = datetime.now()
 engine = create_engine("postgresql+psycopg://localdb:local@localhost:5434/localdb")
-test_points = [12944, 14518, 4410]
-print("Trees in territory:", get_tree_count_in_territory(engine, test_points))
-print("Unique tree types in territory:", len(get_unique_list(engine, test_points, "common_name")))
-print("Number of streets with trees in territory:", len(get_unique_list(engine, test_points, "street_name")))
+test_trees = [12944, 14518, 4410]
+df = get_trees_in_territory(engine, test_trees)
+print("Trees in territory:", len(df.index))
+print("Number of streets with trees in territory:", df["street_name"].nunique())
+print("Unique tree types in territory:", df["common_name"].nunique())
+print("Trees types in territory: ", get_unique_list(df, "common_name"))
+print("Execution time: ", datetime.now() - startTime)
